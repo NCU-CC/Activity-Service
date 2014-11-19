f6a052a3afbc49de0d5ff9991417d20ed3b69708
jasonChiu <jason1122g@gmail.com>
2014-11-19 16:37:10 +0800

    create activity service v2

---
+++ activity-server/src/main/java/tw/edu/ncu/cc/activity/server/webservice/ActivityWebServiceV2.java
@@ -0,0 +1,31 @@
+package tw.edu.ncu.cc.activity.server.webservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import tw.edu.ncu.cc.activity.data.v1.Activity;
+import tw.edu.ncu.cc.activity.server.service.ActivityService;
+
+import java.util.Date;
+import java.util.List;
+
+
+@RestController
+@RequestMapping( method = RequestMethod.GET )
+public class ActivityWebServiceV2 {
+
+    private ActivityService activityService;
+
+    @Autowired
+    public void setActivityService( ActivityService activityService ) {
+        this.activityService = activityService;
+    }
+
+    @RequestMapping( value = "v2/activity" )
+    public List<Activity> getActivities( @RequestParam( "size" ) Integer size ) {
+        return activityService.getLatestActivities( new Date(), ( size > 0 && size <= 40 ? size : 20 ) );
+    }
+
+}
---
+++ activity-server/src/test/groovy/tw/edu/ncu/cc/activity/server/webservice/ActivityWebServiceV2Test.groovy
@@ -0,0 +1,58 @@
+package tw.edu.ncu.cc.activity.server.webservice
+
+import specification.IntegrationSpecification
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+
+class ActivityWebServiceV2Test extends IntegrationSpecification {
+
+    def "it can provide all Activities"() {
+        when:
+            def response = JSON( server()
+                    .perform( get( "/v2/activity?size=10" ).accept( "application/json" ) )
+                    .andExpect( status().isOk() )
+                    .andReturn()
+            );
+        then:
+            response.size() == 3
+            response.contains( JSON(
+                    '''
+                    {
+                        "name"    : "Activity4",
+                        "club"    : "CLUB4",
+                        "place"   : "place4",
+                        "content" : "content4",
+                        "start" : "2050-09-13 09:00",
+                        "end"   : "2050-09-14 23:00"
+                    }
+                    '''
+            ) )
+            response.contains( JSON(
+                    '''
+                    {
+                        "name"    : "Activity5",
+                        "club"    : "CLUB5",
+                        "place"   : "place6",
+                        "content" : "content5",
+                        "start" : "2050-09-14 00:00",
+                        "end"   : "2050-09-15 00:00"
+                    }
+                    '''
+            ) )
+            response.contains( JSON(
+                    '''
+                    {
+                        "name"    : "Activity6",
+                        "club"    : null,
+                        "place"   : "place6",
+                        "content" : "content6",
+                        "start" : "2050-09-15 00:00",
+                        "end"   : "2050-09-16 00:00"
+                    }
+                    '''
+            ) )
+    }
+
+}
